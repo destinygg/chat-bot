d48b6aed2fda706255c403cf7f653f8595727369
johnpyp <20625636+johnpyp@users.noreply.github.com>
2020-10-06 22:59:56 -0400

    Added new message matching service and tests
    Also fixed regex bugs

---
+++ tests/lib/services/message-matching.test.js
@@ -0,0 +1,49 @@
+const { hasLink, mentionsUser } = require('../../../lib/services/message-matching');
+const assert = require('assert');
+
+describe('Message matching tests ', () => {
+  describe('hasLink matches link-containing messages', function() {
+    const goodLinkMessages = [
+      'http://mrlinux.com',
+      'http://test.tv',
+      'https://mrrlinux.dev',
+      'ftp://mrlinux.dev',
+      'ttt https://mrlinux.dev',
+      'ttt https://www.MrLinux.dev/memes',
+      'ttt https://wwwwmrlinux.dev/memes wow epic',
+      'https://mrlinux.dev:80/memes/?query="escaped"',
+      'http://localhost',
+      'http://localhost:80',
+      'http://localhost:4000',
+      'http://memes',
+      'test.tv',
+      '.test.tv',
+      ' testing -- test.tv .test.tv .test.com test.com test..com ...test.com test....test.com test,,,test.com  ,test.com, ',
+      '.test.com.',
+      '...test.com',
+      'test,,,test.com',
+      'cool://mrlinux.dev',
+      'site:https://mrlinux.dev',
+      'https://nice.',
+    ];
+    goodLinkMessages.forEach((msg, i) => {
+      it(`hasLink matches link-containing message #${i + 1}`, () => {
+        assert.deepStrictEqual(hasLink(msg), true, msg);
+      });
+    });
+  });
+  describe('hasLink does not match non-link-containing messages', function() {
+    const badLinkMessages = ['random message in chat.', 'https://.....', 'yeah localhost', 'test.'];
+    badLinkMessages.forEach((msg, i) => {
+      it(`hasLink does not match non-link-containing message #${i + 1}`, () =>
+        assert.deepStrictEqual(hasLink(msg), false, msg));
+    });
+  });
+
+  describe('mentionUser matches actual mentions of a user', function() {
+    it('matches #1', () => assert.deepStrictEqual(mentionsUser('Destiny hi', 'desTiny'), true));
+    it('matches #2', () => assert.deepStrictEqual(mentionsUser('DesTiny hi', 'desTiny'), true));
+    it('matches #3', () =>
+      assert.deepStrictEqual(mentionsUser('destiny hi destiny destiny', 'DESTINY'), true));
+  });
+});
