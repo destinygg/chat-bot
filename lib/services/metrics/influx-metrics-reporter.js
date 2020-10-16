destinygg/chat-bot

commit 25fc6d1fe7adbc448ec73ff8e804df92f6dc2f89

Bump dependencies and node version (#73)
* Get bot running on Node 14

* - Clean up setting the logger for influx
- Catch rejected promises when starting influx reporter, though we aren't doing anything useful with it

* - Auto fixed lint errors
- Ignore the few remaining, some underscore dangle and console usage for the influx metrics reporter

* Bump Dockerfile node image to latest LTS (Nodejs 12)

* Downgrade to twitch-js 1.x for now. 2.x requires a lot of changes

* Don't use util.promisify if we don't have to

* Add .nvmrc file, set to version 14

* Bump node in Dockerfile to 14 as well

* Use 14-slim Docker image requested by Cake

* Bump node version in package.json to 14

* Fix error when no commands are stored in DB and !listcommands is called as the command is multiline output the error messages needs to be put in arrays

lib/services/metrics/influx-metrics-reporter.js
@@ -9,7 +9,7 @@ class InfluxReporter {
       password: config.password,
       database: config.database,
       port: config.port,
-      host: config.host,
+      hosts: [config.host],
       protocol: config.protocol,
       precision: 'm',
     };
@@ -18,17 +18,21 @@ class InfluxReporter {
     // So let's just monkey patch that in.
     const sender = new DefaultSender(dbConfig);
 
-    const oldDiscard = sender.db.pool.discard;
-    const oldText = sender.db.pool.text;
+    // eslint-disable-next-line no-underscore-dangle
+    const oldDiscard = sender.db._pool.discard;
+    // eslint-disable-next-line no-underscore-dangle
+    const oldText = sender.db._pool.text;
 
-    sender.db.pool.text = function internal(props) {
+    // eslint-disable-next-line no-underscore-dangle
+    sender.db._pool.text = function internal(props) {
       // eslint-disable-next-line no-param-reassign
       props.path = config.pathPrefix + props.path;
       // eslint-disable-next-line prefer-rest-params
       return oldText.apply(this, arguments);
     };
 
-    sender.db.pool.discard = function internal(props) {
+    // eslint-disable-next-line no-underscore-dangle
+    sender.db._pool.discard = function internal(props) {
       // eslint-disable-next-line no-param-reassign
       props.path = config.pathPrefix + props.path;
       // eslint-disable-next-line prefer-rest-params
@@ -40,14 +44,13 @@ class InfluxReporter {
       tags,
       unit: MILLISECOND,
       reportInterval: 30000,
-    });
-
-    this.reporter.setLog({
-      info: () => {},
-      trace: () => {},
-      debug: () => {},
-      warn: () => {},
-      error: () => {},
+      log: {
+        info: () => {},
+        trace: () => {},
+        debug: () => {},
+        warn: () => {},
+        error: () => {},
+      },
     });
 
     this.metrics = {};
@@ -60,7 +63,11 @@ class InfluxReporter {
     this.metrics[METRIC_NAMES.COMMANDS_RUN] = this.registry.newCounter(METRIC_NAMES.COMMANDS_RUN);
     this.reporter.addMetricRegistry(this.registry);
 
-    this.reporter.start();
+    this.reporter
+      .start()
+      .then()
+      // eslint-disable-next-line no-console
+      .catch((err) => console.error(err));
   }
 
   incrementCounter(metricName, value) {

.nvmrc
@@ -1,0 +1,1 @@
+v14

Dockerfile
@@ -1,4 +1,4 @@
-FROM node:8.12.0
+FROM node:14-slim
 COPY package.json package.json
 COPY package-lock.json package-lock.json
 RUN npm install

index.js
@@ -38,7 +38,7 @@ services
       services.scheduledCommands,
       services.spamDetection,
       services.logger,
-    ).catch(err => {
+    ).catch((err) => {
       logger.warn(`Problem loading commands/banned phrases from sql. Reason: ${err}`);
     });
   })
@@ -73,13 +73,13 @@ services
     );
     chatServiceRouter.create();
   })
-  .catch(err => {
+  .catch((err) => {
     logger.error('Problem starting up services, shutting down...');
     logger.error(err);
     process.exit(1);
   });
 
-process.on('uncaughtException', err => {
+process.on('uncaughtException', (err) => {
   logger.error('Uncaught exception. Crashing.');
   logger.error(err);
   process.exit(1);

lib/commands/base-punishment-helper.js
@@ -9,7 +9,7 @@ function basePunishmentHelper(input, defaultBanTime, overRideDuration) {
   const usersToPunish = _.get(matched, 2, '')
     .toLowerCase()
     .split(',')
-    .map(x => x.trim());
+    .map((x) => x.trim());
   let parsedDuration = 0;
   let isPermanent = false;
   if (overRideDuration === undefined) {
@@ -37,7 +37,7 @@ function basePunishmentHelper(input, defaultBanTime, overRideDuration) {
   }
 
   const parsedReason = _.get(matched, 3, null);
-  return usersToPunish.map(userToPunish => ({
+  return usersToPunish.map((userToPunish) => ({
     userToPunish,
     parsedDuration,
     parsedReason,

lib/commands/implementations/addcommand.js
@@ -19,7 +19,7 @@ function addCommand(input, services) {
       );
       return new CommandOutput(null, `Added new command: ${newCommand}`);
     })
-    .catch(err => new CommandOutput(err, "Oops. Something didn't work. Check the logs."));
+    .catch((err) => new CommandOutput(err, "Oops. Something didn't work. Check the logs."));
 }
 
 module.exports = new Command(addCommand, true, true, /(!\w+)\s(.*)/);

lib/commands/implementations/aegis.js
@@ -9,7 +9,7 @@ function aegis(input, services) {
   }
   services.punishmentCache.cleaseNukes();
 
-  usersToUnmute.forEach(user => {
+  usersToUnmute.forEach((user) => {
     services.punishmentStream.write(makeUnban(user, false));
   });
 

lib/commands/implementations/ban.js
@@ -42,7 +42,7 @@ function isIpBan(ipBan, defaultBanTime) {
         'Could not parse the duration. Usage: "!ban {amount}{m,h,d,w}OR{perm} {user} {reason}" !mute 1d Destiny he was streaming',
       );
     }
-    parsedInput.forEach(result => {
+    parsedInput.forEach((result) => {
       const { isPermanent, userToPunish, parsedDuration, parsedReason } = result;
       services.punishmentStream.write(
         makeBan(

commit 8da2362e07d9d1bb5f6bf8cf16c8d7020d6b9106

Fix config regression for InfluxDB in #73

lib/services/metrics/influx-metrics-reporter.js
@@ -9,7 +9,7 @@ class InfluxReporter {
       password: config.password,
       database: config.database,
       port: config.port,
-      hosts: [config.host],
+      host: config.host,
       protocol: config.protocol,
       precision: 'm',
     };