698e30f99bc796f79e8992a8fe57ea64df4d4b6e
johnpyp <johnpyp.dev@gmail.com>
2020-12-30 18:35:05 -0500

    Fix punishment time parsing regex to work with 'perm' (#98)
    * Fix punishment time parsing regex to work with 'perm' + test case
    
    * Fix some random broken tests

--- tests/lib/services/message-matching.test.js
+++ tests/lib/services/message-matching.test.js
@@ -2,7 +2,7 @@ const { hasLink, getLinks, mentionsUser } = require('../../../lib/services/messa
 const assert = require('assert');
 
 describe('Message matching tests ', () => {
-  describe('hasLink matches link-containing messages', function() {
+  describe('hasLink matches link-containing messages', function () {
     const goodLinkMessages = [
       'http://mrlinux.com',
       'http://test.tv',
@@ -32,7 +32,7 @@ describe('Message matching tests ', () => {
       });
     });
   });
-  describe('hasLink does not match non-link-containing messages', function() {
+  describe('hasLink does not match non-link-containing messages', function () {
     const badLinkMessages = ['random message in chat.', 'https://.....', 'yeah localhost', 'test.'];
     badLinkMessages.forEach((msg, i) => {
       it(`hasLink does not match non-link-containing message #${i + 1}`, () =>
@@ -40,19 +40,19 @@ describe('Message matching tests ', () => {
     });
   });
 
-  describe('getLinks lists links correctly', function() {
-    const links = ['http://destiny.gg', 'http://twitter.com', 'http://google.com'];
+  describe('getLinks lists links correctly', function () {
+    const links = ['http://destiny.gg/', 'http://twitter.com/', 'http://google.com/'];
     const messageWithLinks = `look at all these pretty links ${links.join(' ')}`;
 
     const foundLinks = getLinks(messageWithLinks);
     links.forEach((link, i) => {
-      it(`getLinks lists links correctly #${i+1}`, () => {
+      it(`getLinks lists links correctly #${i + 1}`, () => {
         assert.deepStrictEqual(foundLinks[i].href, link);
       });
     });
   });
 
-  describe('mentionUser matches actual mentions of a user', function() {
+  describe('mentionUser matches actual mentions of a user', function () {
     it('matches #1', () => assert.deepStrictEqual(mentionsUser('Destiny hi', 'desTiny'), true));
     it('matches #2', () => assert.deepStrictEqual(mentionsUser('DesTiny hi', 'desTiny'), true));
     it('matches #3', () =>
--- tests/lib/services/punishment-read-write-stream.test.js
+++ tests/lib/services/punishment-read-write-stream.test.js
@@ -1,47 +1,49 @@
 const assert = require('assert');
-const PunishmentCache = require('../../../../lib/services/punishment-cache.js');
-const PunishmentReadWriteStream = require('../../../../lib/services/punishment-read-write-stream.js');
+const PunishmentCache = require('../../../lib/services/punishment-cache.js');
+const PunishmentReadWriteStream = require('../../../lib/services/punishment-read-write-stream.js');
 
 describe('Mute capping tests', () => {
-  beforeEach(function() {
+  beforeEach(function () {
     this.mockServices = {
-      punishmentCache: new PunishmentCache({maxMuteSeconds: 100}),
+      punishmentCache: new PunishmentCache({ maxMuteSeconds: 100 }),
     };
   });
 
-  it('limits mute duration to non-negative numbers', function() {
+  it('limits mute duration to non-negative numbers', function () {
     const punishmentReadWriteStream = new PunishmentReadWriteStream(this.mockServices);
     punishmentReadWriteStream.sendMute('mute', -10, 'mute', 'tommy');
     assert.deepStrictEqual(0, punishmentReadWriteStream.read().duration);
   });
 
-  it('does not change the duration when the input duration is exactly 0', function() {
+  it('does not change the duration when the input duration is exactly 0', function () {
     const punishmentReadWriteStream = new PunishmentReadWriteStream(this.mockServices);
     punishmentReadWriteStream.sendMute('mute', 0, 'mute', 'tommy');
     assert.deepStrictEqual(0, punishmentReadWriteStream.read().duration);
   });
 
-  it('does not change the duration when the input duration is between 0 and maxMuteDurationSeconds', function() {
+  it('does not change the duration when the input duration is between 0 and maxMuteDurationSeconds', function () {
     const punishmentReadWriteStream = new PunishmentReadWriteStream(this.mockServices);
     punishmentReadWriteStream.sendMute('mute', 10, 'mute', 'tommy');
     assert.deepStrictEqual(10, punishmentReadWriteStream.read().duration);
   });
 
-  it('does not change the duration when the input duration is exactly maxMuteDurationSeconds', function() {
+  it('does not change the duration when the input duration is exactly maxMuteDurationSeconds', function () {
     const punishmentReadWriteStream = new PunishmentReadWriteStream(this.mockServices);
     punishmentReadWriteStream.sendMute('mute', 100, 'mute', 'tommy');
     assert.deepStrictEqual(100, punishmentReadWriteStream.read().duration);
   });
 
-  it('caps mute duration to maxMuteDurationSeconds', function() {
+  it('caps mute duration to maxMuteDurationSeconds', function () {
     const punishmentReadWriteStream = new PunishmentReadWriteStream(this.mockServices);
     punishmentReadWriteStream.sendMute('mute', 200, 'mute', 'tommy');
     assert.deepStrictEqual(100, punishmentReadWriteStream.read().duration);
   });
 
-  it('throws a TypeError if the mute duration is a string', function() {
+  it('throws a TypeError if the mute duration is a string', function () {
     const punishmentReadWriteStream = new PunishmentReadWriteStream(this.mockServices);
     punishmentReadWriteStream.sendMute('mute', 'OOO', 'mute', 'tommy');
-    assert.throws(() => {punishmentReadWriteStream.sendMute().read(), TypeError});
+    assert.throws(() => {
+      punishmentReadWriteStream.sendMute().read(), TypeError;
+    });
   });
 });
