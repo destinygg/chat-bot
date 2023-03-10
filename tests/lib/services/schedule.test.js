5e41b799ae5b0f699cc09d93258a4df8a5edd650
Farhad Jay <effejaye@gmail.com>
2023-03-10 13:34:44 -0800

    Prettify schedule tests

--- tests/lib/services/schedule.test.js
+++ tests/lib/services/schedule.test.js
@@ -1,49 +1,44 @@
 const assert = require('assert');
 const sinon = require('sinon');
-const proxyquire  = require('proxyquire').noCallThru();
-const mockResponses = require('./mocks/google-calendar-responses.json')
+const proxyquire = require('proxyquire').noCallThru();
+const mockResponses = require('./mocks/google-calendar-responses.json');
 
 describe('Schedule Tests', () => {
-
   const config = {
-      GOOGLE_CALENDAR_API_KEY: 'TEST123',
-      GOOGLE_CALENDAR_ID : 'i54j4cu9pl4270asok3mqgdrhk@group.calendar.google.com'
+    GOOGLE_CALENDAR_API_KEY: 'TEST123',
+    GOOGLE_CALENDAR_ID: 'i54j4cu9pl4270asok3mqgdrhk@group.calendar.google.com',
   };
 
-  const pathStub = function(){
-  }
-  pathStub.calendar = function(config){
+  const pathStub = function () {};
+  pathStub.calendar = function (config) {
     return {
-        events: {
-            list: function(payload){
-                return Promise.resolve({
-                    data: mockResponses.getEventList
-                })
-            }
-        }
-    }
-  }
+      events: {
+        list: function (payload) {
+          return Promise.resolve({
+            data: mockResponses.getEventList,
+          });
+        },
+      },
+    };
+  };
 
-  const scheduleProxy = proxyquire('../../../lib/services/schedule', { 'googleapis': { google: pathStub }})
+  const scheduleProxy = proxyquire('../../../lib/services/schedule', {
+    googleapis: { google: pathStub },
+  });
   const schedule = new scheduleProxy(config);
 
-
   it('Gets a Calendars List Of Events as an Array', function () {
-
-    return schedule.getListOfUpcomingEvents(config.GOOGLE_CALENDAR_ID)
-    .then(function (response) {
-        return assert.equal(Array.isArray(response), true);
+    return schedule.getListOfUpcomingEvents(config.GOOGLE_CALENDAR_ID).then(function (response) {
+      return assert.equal(Array.isArray(response), true);
     });
   });
 
   it('Returns a Calendars Next "Stream" event', function () {
-
-    return schedule.findNextStreamDay()
-    .then(function (response) {
-        return assert.deepStrictEqual(response, {start:{ dateTime: '2018-11-12T17:00:00-06:00',
-            timeZone: 'America/Chicago' },
-          name: 'Stream' });
+    return schedule.findNextStreamDay().then(function (response) {
+      return assert.deepStrictEqual(response, {
+        start: { dateTime: '2018-11-12T17:00:00-06:00', timeZone: 'America/Chicago' },
+        name: 'Stream',
+      });
     });
   });
-
 });
