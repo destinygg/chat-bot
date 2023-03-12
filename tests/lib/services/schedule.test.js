dcf82676aa391a789733962d67f99725ddc3d386
11k <11k@users.noreply.github.com>
2023-03-11 17:57:50 -0800

    Fix `!sch` command (#125)
    * Remove trailing whitespace
    
    * Add rudimentary support for all-day events
    
    * Prettify schedule tests
    
    * Update unit tests
    
    * Format Google calendar mock responses
    
    * Write test for all-day event with alt response set
    
    * Handle scenario where nothing is scheduled
    
    * Use event timezone rather than bot timezone
    
    * Remove unused timezone param

--- tests/lib/services/schedule.test.js
+++ tests/lib/services/schedule.test.js
@@ -1,49 +1,71 @@
 const assert = require('assert');
 const sinon = require('sinon');
-const proxyquire  = require('proxyquire').noCallThru();
-const mockResponses = require('./mocks/google-calendar-responses.json')
+const proxyquire = require('proxyquire').noCallThru();
+const mockResponses = require('./mocks/google-calendar-responses.json');
 
 describe('Schedule Tests', () => {
-
   const config = {
-      GOOGLE_CALENDAR_API_KEY: 'TEST123',
-      GOOGLE_CALENDAR_ID : 'i54j4cu9pl4270asok3mqgdrhk@group.calendar.google.com'
+    GOOGLE_CALENDAR_API_KEY: 'TEST123',
+    GOOGLE_CALENDAR_ID: 'i54j4cu9pl4270asok3mqgdrhk@group.calendar.google.com',
   };
 
-  const pathStub = function(){
-  }
-  pathStub.calendar = function(config){
-    return {
+  const buildSchedule = function (responseSet = mockResponses['getEventList']) {
+    const pathStub = function () {};
+    pathStub.calendar = function (config) {
+      return {
         events: {
-            list: function(payload){
-                return Promise.resolve({
-                    data: mockResponses.getEventList
-                })
-            }
-        }
-    }
-  }
+          list: function (payload) {
+            return Promise.resolve({
+              data: responseSet,
+            });
+          },
+        },
+      };
+    };
 
-  const scheduleProxy = proxyquire('../../../lib/services/schedule', { 'googleapis': { google: pathStub }})
-  const schedule = new scheduleProxy(config);
-
+    const scheduleProxy = proxyquire('../../../lib/services/schedule', {
+      googleapis: { google: pathStub },
+    });
+    return new scheduleProxy(config);
+  };
 
   it('Gets a Calendars List Of Events as an Array', function () {
-
-    return schedule.getListOfUpcomingEvents(config.GOOGLE_CALENDAR_ID)
-    .then(function (response) {
+    return buildSchedule()
+      .getListOfUpcomingEvents(config.GOOGLE_CALENDAR_ID)
+      .then(function (response) {
         return assert.equal(Array.isArray(response), true);
-    });
+      });
   });
 
   it('Returns a Calendars Next "Stream" event', function () {
-
-    return schedule.findNextStreamDay()
-    .then(function (response) {
-        return assert.deepStrictEqual(response, {start:{ dateTime: '2018-11-12T17:00:00-06:00',
-            timeZone: 'America/Chicago' },
-          name: 'Stream' });
-    });
+    return buildSchedule()
+      .findNextStreamDay()
+      .then(function (response) {
+        return assert.deepStrictEqual(response, {
+          start: '2018-11-12T17:00:00-06:00',
+          name: 'Stream',
+          allDay: false,
+        });
+      });
   });
 
+  it('Returns the next all-day calendar event', function () {
+    return buildSchedule(mockResponses['getAllDayEventList'])
+      .findNextStreamDay()
+      .then(function (response) {
+        return assert.deepStrictEqual(response, {
+          start: '2023-03-15',
+          name: 'Stop the Steal Debate with Ali Alexander',
+          allDay: true,
+        });
+      });
+  });
+
+  it('Returns `null` if no events', function () {
+    return buildSchedule({ items: [] })
+      .findNextStreamDay()
+      .then(function (response) {
+        return assert.strictEqual(response, null);
+      });
+  });
 });
