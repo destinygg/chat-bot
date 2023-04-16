bb05c11df9583cadf0d3161ef70b43a67b966f6a
Farhad Jay <effejaye@gmail.com>
2023-04-16 01:51:58 -0700

    Write more tests and fix destructuring/viewer bugs

--- tests/lib/commands/implementations/live.test.js
+++ tests/lib/commands/implementations/live.test.js
@@ -3,6 +3,8 @@ const live = require('../../../../lib/commands/implementations/live');
 const CommandOutput = require('../../../../lib/commands/command-output');
 const assert = require('assert');
 const streamInfoOffline = require('./mocks/stream-info-offline.json');
+const streamInfoOfflineNull = require('./mocks/stream-info-offline-null.json');
+const streamInfoOnline = require('./mocks/stream-info-online.json');
 
 describe('!live Test', () => {
   const buildMockServices = (dggApiResponse) => {
@@ -18,9 +20,23 @@ describe('!live Test', () => {
   it('responds correctly when stream is offline', function () {
     const expected = new CommandOutput(
       null,
-      `Stream was last online 2 days 11h ago. Time Streamed: 4h 34m.`,
+      `Stream was last online 2 days 11h ago. Time Streamed: 4h 35m.`,
     );
     return live.work(null, buildMockServices(streamInfoOffline)).then((response) => {
+      assert.deepStrictEqual(response, expected);
+    });
+  });
+
+  it('responds correctly when all streams are null', function () {
+    const expected = new CommandOutput(null, 'Stream is offline.');
+    return live.work(null, buildMockServices(streamInfoOfflineNull)).then((response) => {
+      assert.deepStrictEqual(response, expected);
+    });
+  });
+
+  it('responds with start time of oldest stream and cumulative viewers', function () {
+    const expected = new CommandOutput(null, `Viewers: 17018. Stream live as of 2 days 16h ago.`);
+    return live.work(null, buildMockServices(streamInfoOnline)).then((response) => {
       assert.deepStrictEqual(response, expected);
     });
   });
