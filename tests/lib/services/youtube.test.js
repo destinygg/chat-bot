838222794f6b80ba2779d3836575cb4e9fb1765a
Farhad Jay <effejaye@gmail.com>
2026-05-24 09:32:28 -0700

    Replace YouTube Data API with destiny.gg public API
    Route the YouTube service through the website's /api/info/videos and
    /api/info/stream endpoints instead of googleapis. Drops the
    YOUTUBE_API_KEY/YOUTUBE_CHANNEL config and shares dggApi.baseUrl across
    both services.

--- tests/lib/services/youtube.test.js
+++ tests/lib/services/youtube.test.js
@@ -1,149 +1,99 @@
 const assert = require('assert');
 const sinon = require('sinon');
-const proxyquire  = require('proxyquire').noCallThru();
-const mockResponses = require('./mocks/youtube-responses.json')
+const proxyquire = require('proxyquire').noCallThru();
 const moment = require('moment');
+const mockResponses = require('./mocks/youtube-responses.json');
 
 describe('Youtube Tests', () => {
+  const config = { baseUrl: 'https://www.destiny.gg' };
 
-  const config = {
-      YOUTUBE_API_KEY: 'TEST123',
-      YOUTUBE_CHANNEL: 'Destiny',
-      liveViewerCountTimeToLiveSeconds: 300
-  };
-
-  const pathStub = function(){
-  }
-  pathStub.youtube = function(config){
-    let searchCalled = 0;
-    let videosCalled = 0;
-    return {
-        channels: {
-            list: function(payload){
-                switch(payload.part.join(',')) {
-                    case 'contentDetails':
-                        return Promise.resolve({ data: mockResponses.getChannelsUploadedPlaylistId });
-                    case 'id':
-                        return Promise.resolve({ data: mockResponses.getChannelIdFromUsername });
-                    default:
-                        return Promise.reject('YOU FUCKED UP');
-                }
-            }
-        },
-        playlistItems: {
-            list: function(){
-                return Promise.resolve({
-                    data: mockResponses.getListOfUploadedVideos
-                });
-            }
-        },
-        search: {
-            list: function(){
-                searchCalled += 1;
-
-                if (searchCalled % 2 === 1) {
-                    return Promise.resolve({ data: mockResponses.getActiveLiveBroadcastsVideoId });
-                }
-                if (searchCalled % 2 === 0) {
-                    return Promise.resolve({ data: mockResponses.getActiveLiveBroadcastsVideoIdOffline });
-                }
-            }
-        },
-        videos: {
-            list: function(){
-                videosCalled += 1;
-
-                if (videosCalled === 2) {
-                    return Promise.resolve({ data: mockResponses.getConcurrentViewersOffline });
-                }
-                return Promise.resolve({ data: mockResponses.getConcurrentViewers });
-            }
-        }
-    }
-  }
-
-  const youtubeProxy = proxyquire('../../../lib/services/youtube', { 'googleapis': { google: pathStub }})
-  const yt = new youtubeProxy(config);
+  let axiosGet;
+  let YouTube;
+  let yt;
 
   beforeEach(function () {
     this.clock = sinon.useFakeTimers(1603155310586);
+    axiosGet = sinon.stub();
+    YouTube = proxyquire('../../../lib/services/youtube', {
+      axios: { default: { get: axiosGet } },
+    });
+    yt = new YouTube(config);
   });
 
   afterEach(function () {
     this.clock.restore();
-    yt.liveViewerCache = {};
   });
 
-  it('Gets a Channels Uploaded Playlist Id', function () {
-
-    return yt.getChannelsUploadedPlaylistId(config.YOUTUBE_CHANNEL)
-    .then(function (response) {
-        return assert.strictEqual(response, "UU554eY5jNUfDq3yDOJYirOQ");
+  it('Gets the channel\'s uploaded videos', function () {
+    axiosGet.withArgs(`${config.baseUrl}/api/info/videos`).resolves({ data: mockResponses.videos });
+    return yt.getListOfUploadedVideos().then((response) => {
+      assert.deepStrictEqual(response, mockResponses.videos.data);
     });
   });
 
-  it('Gets a Channels Uploaded Playlist Id and caches it', function () {
-
-    return yt.getChannelsUploadedPlaylistId(config.YOUTUBE_CHANNEL)
-    .then(function () {
-        return assert.strictEqual(yt.etags['getChannelsUploadedPlaylistId'].data.items[0].contentDetails.relatedPlaylists.uploads, 'UU554eY5jNUfDq3yDOJYirOQ')
+  it('Gets the latest uploaded video', function () {
+    axiosGet.withArgs(`${config.baseUrl}/api/info/videos`).resolves({ data: mockResponses.videos });
+    return yt.getLatestUploadedVideo().then((response) => {
+      assert.deepStrictEqual(response, mockResponses.videos.data[0]);
     });
   });
 
-  it('Gets a Channels Latest Uploaded Videos Playlist', function () {
-
-    return yt.getListOfUploadedVideos()
-    .then(function (response) {
-        return assert.equal(response, mockResponses.getListOfUploadedVideos);
-    });
-  });
-  it('Gets a Channels Latest Uploaded Video', function () {
-
-    return yt.getLatestUploadedVideo()
-    .then(function (response) {
-        return assert.equal(response, mockResponses.getListOfUploadedVideos.items[0]);
+  it('Gets the live broadcast video id when live', function () {
+    axiosGet
+      .withArgs(`${config.baseUrl}/api/info/stream`)
+      .resolves({ data: mockResponses.streamLive });
+    return yt.getActiveLiveBroadcastsVideoId().then((response) => {
+      assert.strictEqual(response, 'qif_XUayrWY');
     });
   });
 
-  it('Gets the channel id from username', function() {
-      return yt.getChannelIdFromUsername(config.YOUTUBE_CHANNEL)
-      .then(function (response) {
-          return assert.strictEqual(response, 'UC554eY5jNUfDq3yDOJYirOQ')
+  it('Gets the live broadcast video id as null when offline', function () {
+    axiosGet
+      .withArgs(`${config.baseUrl}/api/info/stream`)
+      .resolves({ data: mockResponses.streamOffline });
+    return yt.getActiveLiveBroadcastsVideoId().then((response) => {
+      assert.strictEqual(response, null);
+    });
+  });
+
+  it('Gets the live broadcast video id as null when youtube stream is missing', function () {
+    axiosGet
+      .withArgs(`${config.baseUrl}/api/info/stream`)
+      .resolves({ data: mockResponses.streamNoYoutube });
+    return yt.getActiveLiveBroadcastsVideoId().then((response) => {
+      assert.strictEqual(response, null);
+    });
+  });
+
+  it('Gets channel status with viewers when live', function () {
+    axiosGet
+      .withArgs(`${config.baseUrl}/api/info/stream`)
+      .resolves({ data: mockResponses.streamLive });
+    return yt.getChannelStatus().then((response) => {
+      assert.deepStrictEqual(response, {
+        timestamp: 1603155310,
+        isLive: true,
+        viewers: 1785,
+        started: moment('2020-10-10T19:04:28Z', 'YYYY-MM-DDTHH:mm:ssZ'),
       });
-  });
-
-  it('Gets the channel id from username and caches it', function() {
-    return yt.getChannelIdFromUsername(config.YOUTUBE_CHANNEL)
-    .then(function () {
-        return assert.strictEqual(yt.etags['getChannelIdFromUsername'].data.items[0].id, 'UC554eY5jNUfDq3yDOJYirOQ')
-    });
-});
-
-  it('Gets live broadcast details from channel id', function() {
-    return yt.getActiveLiveBroadcastsVideoId()
-    .then(function (response) {
-        return assert.strictEqual(response, 'qif_XUayrWY');
     });
   });
 
-  it('Gets live broadcast details from channel id when offline', function() {
-    return yt.getActiveLiveBroadcastsVideoId()
-    .then(function (response) {
-        return assert.strictEqual(response, null);
+  it('Gets channel status as offline when not live', function () {
+    axiosGet
+      .withArgs(`${config.baseUrl}/api/info/stream`)
+      .resolves({ data: mockResponses.streamOffline });
+    return yt.getChannelStatus().then((response) => {
+      assert.deepStrictEqual(response, { timestamp: 1603155310, isLive: false });
     });
   });
 
-  it('Gets live broadcast concurrent viewers count', function() {
-    return yt.getChannelStatus()
-    .then(function (response) {
-        return assert.deepStrictEqual(response, { timestamp: 1603155310, isLive: true, viewers: '1785', started: moment('2020-10-10T19:04:28Z', 'YYYY-MM-DDTHH:mm:ssZ') });
-    });
-  });
-
-  it('Gets live broadcast concurrent viewers count when offline', function() {
-    return yt.getChannelStatus()
-    .then(function (response) {
-        return assert.deepStrictEqual(response, { timestamp: 1603155310, isLive: false });
+  it('Gets channel status as offline when youtube stream is missing', function () {
+    axiosGet
+      .withArgs(`${config.baseUrl}/api/info/stream`)
+      .resolves({ data: mockResponses.streamNoYoutube });
+    return yt.getChannelStatus().then((response) => {
+      assert.deepStrictEqual(response, { timestamp: 1603155310, isLive: false });
     });
   });
 });
