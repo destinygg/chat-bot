95e69fa54ad3b86020d7d241d1163c50d21efa68
Farhad Jay <effejaye@gmail.com>
2026-05-24 09:49:58 -0700

    Fold YouTube service into DggApi
    Both services now hit the same destiny.gg base URL with no special
    state, so collapse the video methods onto DggApi and delete the
    standalone YouTube service.

--- tests/lib/services/dgg-api.test.js
+++ tests/lib/services/dgg-api.test.js
@@ -1,33 +1,33 @@
 const assert = require('assert');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire').noCallThru();
-const mockResponses = require('./mocks/youtube-responses.json');
+const mockResponses = require('./mocks/dgg-api-responses.json');
 
-describe('Youtube Tests', () => {
+describe('DggApi Tests', () => {
   const config = { baseUrl: 'https://www.destiny.gg' };
 
   let axiosGet;
-  let YouTube;
-  let yt;
+  let DggApi;
+  let dggApi;
 
   beforeEach(function () {
     axiosGet = sinon.stub();
-    YouTube = proxyquire('../../../lib/services/youtube', {
+    DggApi = proxyquire('../../../lib/services/dgg-api', {
       axios: { default: { get: axiosGet } },
     });
-    yt = new YouTube(config);
+    dggApi = new DggApi(config);
   });
 
   it('Gets the channel\'s uploaded videos', function () {
     axiosGet.withArgs(`${config.baseUrl}/api/info/videos`).resolves({ data: mockResponses.videos });
-    return yt.getListOfUploadedVideos().then((response) => {
+    return dggApi.getListOfUploadedVideos().then((response) => {
       assert.deepStrictEqual(response, mockResponses.videos.data);
     });
   });
 
   it('Gets the latest uploaded video', function () {
     axiosGet.withArgs(`${config.baseUrl}/api/info/videos`).resolves({ data: mockResponses.videos });
-    return yt.getLatestUploadedVideo().then((response) => {
+    return dggApi.getLatestUploadedVideo().then((response) => {
       assert.deepStrictEqual(response, mockResponses.videos.data[0]);
     });
   });
